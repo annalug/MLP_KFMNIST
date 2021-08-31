9276b411f5fe7999a6ebd847dcd1d77c33bb7f81
Anna Luiza Gomes <annalgs@id.uff.br>
2021-08-31 12:20:33 -0300

    correcting the folder

---
+++ mlp_app.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+from Pipeline import Pipeline
+from neural_reg import NeuralNetwork_reg
+from neural import NeuralNetwork
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title('Multilayer perceptron with MNIST,FMNIST and KMNIST')
+
+
+optmizers = ['adamax','adam','asgd']
+lr_rates = np.arange(0.001,0.01,0.003)
+datasets = ['MNIST','FashionMNIST','KMNIST']
+
+datasets = st.selectbox('Select the dataset', datasets)
+lr_rates = st.selectbox('Select the learning rate', lr_rates)
+optmizers = st.selectbox('Select the optmizer', optmizers)
+
+
+acc, train_loss, test_loss = Pipeline(dataset=datasets,lr_rate=lr_rates, random_perspective=True, distortion_scale=0.8, optimizer=optmizers)
